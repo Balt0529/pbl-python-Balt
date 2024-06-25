9a6db3ecfa72c1b5f7428a20c6bbb74b3b6309ec
Balt0529 <kamakiri2255@gmail.com>
2024-06-25 18:00:14 +0900

    変更した

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from typing import Union
-
 from fastapi import FastAPI
 from pydantic import BaseModel
+from database import read_weather
 
 app=FastAPI()
 
@@ -21,3 +21,8 @@ def read_item(item_id:int, q:Union[str,None]=None):
 @app.put("/items/{item_id}")
 def update_item(item_id:int,item:Item):
     return{"item_name":item.name,"item_id":item_id}
+
+@app.get("/users")
+def get_users():
+    get_weathers=read_weather()
+    return get_weathers
